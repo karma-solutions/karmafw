5c14cecd64718fe2ba0a610fb995c6aed666a168
Max F <max@karma-solutions.fr>
2020-11-11 00:22:23 +0100

    dev 2020-11

--- src/Database/Redis/Redis.php
+++ src/Database/Redis/Redis.php
@@ -9,9 +9,10 @@ class Redis
 {
 	protected $dsn = null;
 	protected $client = null;
+	protected $connection_name = null;
 	
 
-	public function __construct($redis_dsn=null)
+	public function __construct($redis_dsn=null, $connection_name='')
 	{
 		if (empty($redis_dsn) && defined('REDIS_DSN')) {
 			$redis_dsn = REDIS_DSN;
@@ -24,28 +25,38 @@ class Redis
 		if (class_exists('\\Predis\\Client')) {
 			$this->setClient(new Predis\Client($redis_dsn));
 			$this->dsn = $redis_dsn;
-		}
+			$this->connection_name = $connection_name;
 
+			if ($connection_name) {
+				$this->getClient()->client('SETNAME', $connection_name);
+			}
+
+		}
 	}
+
 
 	public function getClient()
 	{
 		return $this->client;
 	}
 
+
 	public function setClient($client)
 	{
 		$this->client = $client;
 	}
+
 
 	public function get($key)
 	{
 		return $this->client->get($key);
 	}
 
+
 	public function set($key, $value)
 	{
 		return $this->client->set($key, $value);
 	}
+	
 }
 
