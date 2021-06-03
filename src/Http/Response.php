d879fc3d421f6df2058af2b78fc9e68bd9bc3382
Max F <max@karma-solutions.fr>
2021-06-03 15:56:46 +0200

    dev 2021-06

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -313,9 +313,10 @@ class Response
 
 				if (! is_file($this->download_file_path)) {
 					// File not found
+					$download_file_name = $this->download_file_name;
 					$this->download_file_name = null;
 					$this->download_file_path = null;
-					return $this->html("File '" . $this->download_file_name . "' not found", 404);
+					return $this->html("File '" . $download_file_name . "' not found", 404);
 
 				} else {
 					// File exists
